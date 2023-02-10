f0a2bb56fc7ca99e04629cacfb3686e5cd5eed69
Sasha Aleshchenko <45843308+TheBigSasha@users.noreply.github.com>
2023-02-10 01:13:26 -0500

    Remove template stuff

--- components/shared/Layout.tsx
+++ components/shared/Layout.tsx
@@ -1,7 +1,6 @@
 import { Footer } from 'components/global/Footer'
 import { Navbar } from 'components/global/Navbar'
 import { PreviewBanner } from 'components/preview/PreviewBanner'
-import IntroTemplate from 'intro-template'
 import { SettingsPayload } from 'types'
 
 const fallbackSettings: SettingsPayload = {
@@ -26,7 +25,6 @@ export default function Layout({
       <Navbar menuItems={settings?.menuItems} />
       <div className="mt-20 flex-grow px-4 md:px-16 lg:px-32">{children}</div>
       <Footer footer={settings?.footer} />
-      <IntroTemplate />
     </div>
   )
 }
